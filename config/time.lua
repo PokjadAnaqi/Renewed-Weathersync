aa89bfac432831461a34e2b6997c18e6765455db
PokjadAnaqi <alypheyjad1@gmail.com>
2026-01-15 04:44:49 +0800

    Update time.lua

--- config/time.lua
+++ config/time.lua
@@ -1,18 +1,18 @@
 return {
-    timeScale = 4000, -- How many milliseconds per minute in GTA time (2000ms is normal for a 48 minute day)
+    timeScale = 3500, -- How many milliseconds per minute in GTA time (2000ms is normal for a 48 minute day)
 
-    useNightScale = false, -- If true, the server will use timeScaleNight to alter the time during night
-    timeScaleNight = 8000, -- How many milliseconds per minute in GTA time DURING NIGHT
+    useNightScale = true, -- If true, the server will use timeScaleNight to alter the time during night
+    timeScaleNight = 7000, -- How many milliseconds per minute in GTA time DURING NIGHT
 
     nightTime = {
-        beginning = 22,
+        beginning = 21,
         ending = 6
     },
 
     useRealTime = false, -- If true, the server will override all other configs and use the servers real time.
 
     startUpTime = {
-        hour = 12,
+        hour = 7,
         minute = 0
     }
 }
